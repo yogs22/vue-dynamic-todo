3e284c94d1884d2d20bb1710cb9f672047a137b1
liloo2040 <e.dufour.89@gmail.com>
2018-10-08 18:24:52 +0200

    adding css on form and info

--- index.php
+++ index.php
@@ -5,16 +5,16 @@
 	<meta charset="utf-8">
 	<link rel="stylesheet" type="text/css" href="bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="main.css">
-	<link href="https://fonts.googleapis.com/css?family=Bai+Jamjuree" rel="stylesheet"> 
-	<link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet"> 
+	<link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Bai+Jamjuree" rel="stylesheet">  
 </head>
 <body>
 	<div id="app">
 		<div class="container">
-			<h1>Vue Todo List</h1>
-				<input type="text" v-model="newTodo" class="form-control col-md-4 offset-md-4" @keyup.enter="addTodo" placeholder="Enter your todo">
+			<h1 class="title">Vue Todo List</h1>
+				<input type="text" v-model="newTodo" class="form-control col-md-4 offset-md-4 form" @keyup.enter="addTodo" placeholder="Enter your todo">
 			<ul>
-				<p v-if="!todos.length" class="notodo">No ToDo</p>
+				<p v-if="!todos.length" class="col-md-4 offset-md-4 notodo"> No ToDo</p>
 				<li v-for="(todo, index) in todos">
 					<span :class="{'completed': (typeof todo.done === 'string') ? parseInt(todo.done) : todo.done }">{{ todo.text }}<span> &nbsp;
 					<button class="btn btn-danger btn-sm" @click="removeTodo(index, todo.id_date)">X</button>
